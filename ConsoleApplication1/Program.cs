55b02c7c5da54767bfa158e256bd26a4fef84cde
paradise999 <32838010+paradise999@users.noreply.github.com>
2018-04-18 03:04:03 +0300

    Initial commit

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hanoi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            char from = 'A', to = 'B', help = 'C';
+
+            do
+            {
+                try
+                {
+                    Console.Write("  input number of disk: ");
+                    x = Int32.Parse(Console.ReadLine());
+                }
+                catch (FormatException e)
+                {
+                    x = -10;
+                }
+            } while (x == -10 || x > 10);
+            Console.WriteLine("\n  from = A, to = B, help = C\n");
+            hanoi(x, from, to, help);
+
+            Console.Read();
+        }
+
+        static void hanoi(int x, char from, char to, char help)
+        {
+            if (x > 0)
+            {
+                hanoi(x - 1, from, help, to);
+                move(x, from, to);
+                hanoi(x - 1, help, to, from);
+            }
+
+        }
+
+        static void move(int x, char from, char to)
+        {
+            Console.WriteLine("  move disk " + x + " from " + from + " to " + to);
+        }
+
+    }
+}
